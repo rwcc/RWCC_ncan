f0f2691a1ac575c0135685fc4cb2145a6130cce8
Wojtowicz <Wojt03@osumc.edu>
2017-05-05 14:37:43 -0400

    Add project files.

---
+++ RWCC_ncan/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RWCC_ncan.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
---
+++ RWCC_ncan/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RWCC_ncan.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
